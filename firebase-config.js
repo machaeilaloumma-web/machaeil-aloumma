3cf33dcf16fb55c800cce60a2356a162d76eb948
machaeilaloumma-web <machaeil.aloumma@gmail.com>
2025-10-31 00:33:32 +0100

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCnfl6xuzmPCgHBgNE1XswbcQdR1AflyKU",
+  authDomain: "machaeil-oumma-v2.firebaseapp.com",
+  projectId: "machaeil-oumma-v2",
+  storageBucket: "machaeil-oumma-v2.firebasestorage.app",
+  messagingSenderId: "655616772509",
+  appId: "1:655616772509:web:165cafc18d9286598fc774"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
